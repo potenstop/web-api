caaf96daef7c1bd3be21bc2f75682a9236a81b01
potenstop <potenstop@163.com>
2019-06-23 18:42:49 +0800

    实现注册接口

---
+++ src/main/java/top/potens/web/dao/db/auto/MemberAuthMapper.java
@@ -0,0 +1,22 @@
+package top.potens.web.dao.db.auto;
+
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+import top.potens.web.model.MemberAuth;
+import top.potens.web.model.MemberAuthExample;
+
+public interface MemberAuthMapper {
+    long countByExample(MemberAuthExample example);
+
+    int deleteByExample(MemberAuthExample example);
+
+    int insert(MemberAuth record);
+
+    int insertSelective(MemberAuth record);
+
+    List<MemberAuth> selectByExample(MemberAuthExample example);
+
+    int updateByExampleSelective(@Param("record") MemberAuth record, @Param("example") MemberAuthExample example);
+
+    int updateByExample(@Param("record") MemberAuth record, @Param("example") MemberAuthExample example);
+}
